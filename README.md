aa360d7ce197a7d7a233ed676fa3c4ab2f7fc331
paras231 <sharmaparas553@gmail.com>
2024-06-15 14:18:59 +0530

    added python chatbot using nltk

---
+++ README.md
@@ -0,0 +1,3 @@
+Followed this article for this code :
+
+https://www.analyticsvidhya.com/blog/2021/07/build-a-simple-chatbot-using-python-and-nltk/#h-building-rule-based-chatbot-using-python-nltk-library
---
+++ bot.py
@@ -0,0 +1,13 @@
+print("running python in wsl")
+import nltk
+from nltk.chat.util import Chat, reflections
+from pairs import pairs
+
+
+def chat():
+    print("Hi! I am a chatbot created by Analytics Vidhya for your service")
+    chat = Chat(pairs, reflections)
+    chat.converse()
+#initiate the conversation
+if __name__ == "__main__":
+    chat()
